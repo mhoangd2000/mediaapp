a69fa6cf338669794577b057abc66ff7c57333e9
mhoangd2000 <mhoangd2000@gmail.com>
2024-06-26 18:01:59 +0700

    fix overflowed register page

--- lib/page/register_page.dart
+++ lib/page/register_page.dart
@@ -98,80 +98,85 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "M I N I M A L",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.inversePrimary,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(
+                25.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 120),
+                  Text(
+                    "M I N I M A L",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // user name textfield
-                MyTextFieldd(
-                    hintText: 'user name',
-                    obscureText: false,
-                    controller: userNameController),
-                const SizedBox(
-                  height: 20,
-                ),
-                // email textField
-                MyTextFieldd(
-                    hintText: 'email',
-                    obscureText: false,
-                    controller: emailController),
-                const SizedBox(
-                  height: 20,
-                ),
-                // password
-                MyTextFieldd(
-                    hintText: 'password ',
-                    obscureText: true,
-                    controller: passwordController),
-                const SizedBox(
-                  height: 20,
-                ),
-                //confirm password
-                MyTextFieldd(
-                    hintText: 'confirm password',
-                    obscureText: true,
-                    controller: confirmPwController),
-                const SizedBox(
-                  height: 20,
-                ),
-                // register password
-                MyButton(
-                  text: 'Register',
-                  onTap: registerUser,
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // user name textfield
+                  MyTextFieldd(
+                      hintText: 'user name',
+                      obscureText: false,
+                      controller: userNameController),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // email textField
+                  MyTextFieldd(
+                      hintText: 'email',
+                      obscureText: false,
+                      controller: emailController),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // password
+                  MyTextFieldd(
+                      hintText: 'password ',
+                      obscureText: true,
+                      controller: passwordController),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //confirm password
+                  MyTextFieldd(
+                      hintText: 'confirm password',
+                      obscureText: true,
+                      controller: confirmPwController),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // register password
+                  MyButton(
+                    text: 'Register',
+                    onTap: registerUser,
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Already have an account '),
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child: const Text(
-                        'Login here',
-                        style: TextStyle(
-                          color: Colors.blue,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Already have an account '),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: const Text(
+                          'Login here',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
